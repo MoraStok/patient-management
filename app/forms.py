78fab21eff11ef00bd821ed7f3e45fe845918f95
MoraStok <morastok@gmail.com>
2025-01-22 13:38:57 +0100

    refactoring custom user form

--- app/forms.py
+++ app/forms.py
@@ -16,28 +16,34 @@ from .models import (
     Message
     )
 
-class StaffUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
     password2 = forms.CharField(
         label="Password confirmation", widget=forms.PasswordInput
     )
 
     class Meta:
-        model = Staff
-        fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'password1', 'password2', 'license']
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'password1', 'password2']
         widgets = {
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         }
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs.update({'placeholder': 'First Name', 'class': 'form-control'})
         self.fields['last_name'].widget.attrs.update({'placeholder': 'Last Name', 'class': 'form-control'})
         self.fields['date_of_birth'].widget.attrs.update({'placeholder': 'Date of Birth', 'class': 'form-control'})
         self.fields['email'].widget.attrs.update({'placeholder': 'Email Address', 'class': 'form-control'})
-        self.fields['license'].widget.attrs.update({'placeholder': 'License', 'class': 'form-control'})
         self.fields['password1'].widget.attrs.update({'placeholder': 'Password', 'class': 'form-control'})
         self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Password', 'class': 'form-control'})
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -47,15 +53,27 @@ class StaffUserCreationForm(UserCreationForm):
             user.save()
         return user
 
-class PatientUserCreationForm(UserCreationForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
+class StaffUserCreationForm(CustomUserCreationForm):
+    class Meta:
+        model = Staff
+        fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'password1', 'password2', 'license']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['license'].widget.attrs.update({'placeholder': 'License', 'class': 'form-control'})
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.role = 'doctor'
+        if commit:
+            user.save()
+        return user
 
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label="Password confirmation", widget=forms.PasswordInput
-    )
-
+class PatientUserCreationForm(CustomUserCreationForm):
     class Meta:
         model = Patient
         fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'password1', 'password2', 'social_sec_number']
@@ -65,22 +83,7 @@ class PatientUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Adding placeholders and CSS classes for better styling
-        self.fields['first_name'].widget.attrs.update({'placeholder': 'First Name', 'class': 'form-control'})
-        self.fields['last_name'].widget.attrs.update({'placeholder': 'Last Name', 'class': 'form-control'})
-        self.fields['date_of_birth'].widget.attrs.update({'placeholder': 'Date of Birth', 'class': 'form-control'})
-        self.fields['email'].widget.attrs.update({'placeholder': 'Email Address', 'class': 'form-control'})
         self.fields['social_sec_number'].widget.attrs.update({'placeholder': 'Social Security Number', 'class': 'form-control'})
-        self.fields['password1'].widget.attrs.update({'placeholder': 'Password', 'class': 'form-control'})
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Password', 'class': 'form-control'})
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords don't match")
-        return password2
 
     def save(self, commit=True):
         user = super().save(commit=False)
